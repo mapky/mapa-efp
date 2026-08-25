96155779264ecfe284e85e38d2587b05d3799f9d
robertsasik <robertsasik@gmail.com>
2026-08-25 15:48:48 +0200

    Porastova mapa +

--- layers/layers.js
+++ layers/layers.js
@@ -45,43 +45,39 @@ var lyr_DMR50TieovanrelifZBGISGK_1 = new ol.layer.Tile({
                 url: 'https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.pn'
             })
         });
-var format_Ekologickofunknpriestory_3 = new ol.format.GeoJSON();
-var features_Ekologickofunknpriestory_3 = format_Ekologickofunknpriestory_3.readFeatures(json_Ekologickofunknpriestory_3, 
+
+        var lyr_PorastovmapaNLC_3 = new ol.layer.Tile({
+            'title': 'Porastová mapa NLC',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://gis.nlcsk.org/mgs/rest/services/BackgroundMaps/Porastova_mapa_web/MapServer/tile/{z}/{y}/{x}'
+            })
+        });
+var format_Ekologickofunknpriestory_4 = new ol.format.GeoJSON();
+var features_Ekologickofunknpriestory_4 = format_Ekologickofunknpriestory_4.readFeatures(json_Ekologickofunknpriestory_4, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_Ekologickofunknpriestory_3 = new ol.source.Vector({
+var jsonSource_Ekologickofunknpriestory_4 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_Ekologickofunknpriestory_3.addFeatures(features_Ekologickofunknpriestory_3);
-var lyr_Ekologickofunknpriestory_3 = new ol.layer.Vector({
+jsonSource_Ekologickofunknpriestory_4.addFeatures(features_Ekologickofunknpriestory_4);
+var lyr_Ekologickofunknpriestory_4 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_Ekologickofunknpriestory_3, 
-                style: style_Ekologickofunknpriestory_3,
+                source:jsonSource_Ekologickofunknpriestory_4, 
+                style: style_Ekologickofunknpriestory_4,
                 popuplayertitle: 'Ekologicko-funkčné priestory',
                 interactive: true,
     title: 'Ekologicko-funkčné priestory<br />\
-    <img src="styles/legend/Ekologickofunknpriestory_3_0.png" /> Lesy - Bezzásah<br />\
-    <img src="styles/legend/Ekologickofunknpriestory_3_1.png" /> Lesy - Prírode blízke hospodárenie<br />\
-    <img src="styles/legend/Ekologickofunknpriestory_3_2.png" /> Lesy - Hluchánia oblasť<br />\
-    <img src="styles/legend/Ekologickofunknpriestory_3_3.png" /> Lesy - Bežné hospodárenie<br />\
-    <img src="styles/legend/Ekologickofunknpriestory_3_4.png" /> Trvalé trávne porasty<br />\
-    <img src="styles/legend/Ekologickofunknpriestory_3_5.png" /> Rašeliniská a mokrade<br />\
-    <img src="styles/legend/Ekologickofunknpriestory_3_6.png" /> Brehové porasty a vodné toky<br />\
-    <img src="styles/legend/Ekologickofunknpriestory_3_7.png" /> Ostatné plochy<br />' });
-var format_JPRLCHKOKysuce_4 = new ol.format.GeoJSON();
-var features_JPRLCHKOKysuce_4 = format_JPRLCHKOKysuce_4.readFeatures(json_JPRLCHKOKysuce_4, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_JPRLCHKOKysuce_4 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_JPRLCHKOKysuce_4.addFeatures(features_JPRLCHKOKysuce_4);
-var lyr_JPRLCHKOKysuce_4 = new ol.layer.Vector({
-                declutter: false,
-                source:jsonSource_JPRLCHKOKysuce_4, 
-                style: style_JPRLCHKOKysuce_4,
-                popuplayertitle: 'JPRL CHKO Kysuce',
-                interactive: true,
-                title: '<img src="styles/legend/JPRLCHKOKysuce_4.png" /> JPRL CHKO Kysuce'
-            });
+    <img src="styles/legend/Ekologickofunknpriestory_4_0.png" /> Lesy - Bezzásah<br />\
+    <img src="styles/legend/Ekologickofunknpriestory_4_1.png" /> Lesy - Prírode blízke hospodárenie<br />\
+    <img src="styles/legend/Ekologickofunknpriestory_4_2.png" /> Lesy - Hluchánia oblasť<br />\
+    <img src="styles/legend/Ekologickofunknpriestory_4_3.png" /> Lesy - Bežné hospodárenie<br />\
+    <img src="styles/legend/Ekologickofunknpriestory_4_4.png" /> Trvalé trávne porasty<br />\
+    <img src="styles/legend/Ekologickofunknpriestory_4_5.png" /> Rašeliniská a mokrade<br />\
+    <img src="styles/legend/Ekologickofunknpriestory_4_6.png" /> Brehové porasty a vodné toky<br />\
+    <img src="styles/legend/Ekologickofunknpriestory_4_7.png" /> Ostatné plochy<br />' });
 var format_HraniceCHKOKysuce_5 = new ol.format.GeoJSON();
 var features_HraniceCHKOKysuce_5 = format_HraniceCHKOKysuce_5.readFeatures(json_HraniceCHKOKysuce_5, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
@@ -113,18 +109,15 @@ var lyr_zemiaeurpskehovznamu_6 = new ol.layer.Vector({
                 title: '<img src="styles/legend/zemiaeurpskehovznamu_6.png" /> Územia európskeho významu'
             });
 
-lyr_OrtofototosnmkyZBGISGK_0.setVisible(false);lyr_DMR50TieovanrelifZBGISGK_1.setVisible(true);lyr_PodkladovmapaCartoLight_2.setVisible(true);lyr_Ekologickofunknpriestory_3.setVisible(true);lyr_JPRLCHKOKysuce_4.setVisible(false);lyr_HraniceCHKOKysuce_5.setVisible(true);lyr_zemiaeurpskehovznamu_6.setVisible(true);
-var layersList = [lyr_OrtofototosnmkyZBGISGK_0,lyr_DMR50TieovanrelifZBGISGK_1,lyr_PodkladovmapaCartoLight_2,lyr_Ekologickofunknpriestory_3,lyr_JPRLCHKOKysuce_4,lyr_HraniceCHKOKysuce_5,lyr_zemiaeurpskehovznamu_6];
-lyr_Ekologickofunknpriestory_3.set('fieldAliases', {'fid': 'fid', 'OBJECTID': 'OBJECTID', 'KOD_UEV': 'KOD_UEV', 'NAZOV_UEV': 'NAZOV_UEV', 'POSOBNOST': 'POSOBNOST', 'na_vode': 'na_vode', 'EFP': 'Ekologicko-funkčné plochy', 'Manazment': 'Menežmentové opatrenia', 'Biotop_spo': 'Biotop', });
-lyr_JPRLCHKOKysuce_4.set('fieldAliases', {'fid': 'fid', 'Plocha': 'Plocha v ha', 'idjprl': 'ID JPRL', 'CLHC': 'Číslo LHC', 'LHC': 'Lesný hospodársky celok', 'datum': 'datum', 'VEK': 'Vek porastu', 'Druh_pozem': 'Druh pozemku', 'COLP': 'Číslo lesného pozemku', 'Výmera_JPR': 'Výmera_JPR', 'OBH': 'Obhospodarovateľ', 'KodOBH': 'Kód obhospodarovateľa', 'Adresa_OBH': 'Adresa obhospodarovateľa', 'OLH': 'Odborný lesný hospodár', 'KodOLH': 'Kód OLH', 'Shape_Leng': 'Shape_Leng', 'Shape_Area': 'Shape_Area', });
+lyr_OrtofototosnmkyZBGISGK_0.setVisible(false);lyr_DMR50TieovanrelifZBGISGK_1.setVisible(true);lyr_PodkladovmapaCartoLight_2.setVisible(true);lyr_PorastovmapaNLC_3.setVisible(false);lyr_Ekologickofunknpriestory_4.setVisible(true);lyr_HraniceCHKOKysuce_5.setVisible(true);lyr_zemiaeurpskehovznamu_6.setVisible(true);
+var layersList = [lyr_OrtofototosnmkyZBGISGK_0,lyr_DMR50TieovanrelifZBGISGK_1,lyr_PodkladovmapaCartoLight_2,lyr_PorastovmapaNLC_3,lyr_Ekologickofunknpriestory_4,lyr_HraniceCHKOKysuce_5,lyr_zemiaeurpskehovznamu_6];
+lyr_Ekologickofunknpriestory_4.set('fieldAliases', {'fid': 'fid', 'OBJECTID': 'OBJECTID', 'KOD_UEV': 'KOD_UEV', 'NAZOV_UEV': 'NAZOV_UEV', 'POSOBNOST': 'POSOBNOST', 'na_vode': 'na_vode', 'EFP': 'Ekologicko-funkčné plochy', 'Manazment': 'Menežmentové opatrenia', 'Biotop_spo': 'Biotop', });
 lyr_HraniceCHKOKysuce_5.set('fieldAliases', {'HECTARES': 'HECTARES', 'NAZOV': 'NAZOV', 'VYM_CHKO': 'VYM_CHKO', 'VYM_HA': 'VYM_HA', 'AREA': 'AREA', 'PERIMETER': 'PERIMETER', });
 lyr_zemiaeurpskehovznamu_6.set('fieldAliases', {'fid': 'fid', 'OBJECTID': 'OBJECTID', 'KOD_UEV': 'Kód ÚEV', 'NAZOV_UEV': 'Názov ÚEV', 'POSOBNOST': 'Pôsobnosť', 'VYMERA_HA': 'Výmera v ha', 'Shape_Leng': 'Shape_Leng', 'Shape_Area': 'Shape_Area', 'na_vode': 'na_vode', });
-lyr_Ekologickofunknpriestory_3.set('fieldImages', {'fid': 'TextEdit', 'OBJECTID': 'TextEdit', 'KOD_UEV': 'TextEdit', 'NAZOV_UEV': 'TextEdit', 'POSOBNOST': 'TextEdit', 'na_vode': 'TextEdit', 'EFP': 'TextEdit', 'Manazment': 'TextEdit', 'Biotop_spo': 'TextEdit', });
-lyr_JPRLCHKOKysuce_4.set('fieldImages', {'fid': 'TextEdit', 'Plocha': 'TextEdit', 'idjprl': 'TextEdit', 'CLHC': 'TextEdit', 'LHC': 'TextEdit', 'datum': 'DateTime', 'VEK': 'TextEdit', 'Druh_pozem': 'TextEdit', 'COLP': 'TextEdit', 'Výmera_JPR': 'TextEdit', 'OBH': 'TextEdit', 'KodOBH': 'TextEdit', 'Adresa_OBH': 'TextEdit', 'OLH': 'TextEdit', 'KodOLH': 'TextEdit', 'Shape_Leng': 'TextEdit', 'Shape_Area': 'TextEdit', });
+lyr_Ekologickofunknpriestory_4.set('fieldImages', {'fid': 'TextEdit', 'OBJECTID': 'TextEdit', 'KOD_UEV': 'TextEdit', 'NAZOV_UEV': 'TextEdit', 'POSOBNOST': 'TextEdit', 'na_vode': 'TextEdit', 'EFP': 'TextEdit', 'Manazment': 'TextEdit', 'Biotop_spo': 'TextEdit', });
 lyr_HraniceCHKOKysuce_5.set('fieldImages', {'HECTARES': 'TextEdit', 'NAZOV': 'TextEdit', 'VYM_CHKO': 'TextEdit', 'VYM_HA': 'TextEdit', 'AREA': 'TextEdit', 'PERIMETER': 'TextEdit', });
 lyr_zemiaeurpskehovznamu_6.set('fieldImages', {'fid': 'TextEdit', 'OBJECTID': 'TextEdit', 'KOD_UEV': 'TextEdit', 'NAZOV_UEV': 'TextEdit', 'POSOBNOST': 'TextEdit', 'VYMERA_HA': 'TextEdit', 'Shape_Leng': 'TextEdit', 'Shape_Area': 'TextEdit', 'na_vode': 'TextEdit', });
-lyr_Ekologickofunknpriestory_3.set('fieldLabels', {'fid': 'hidden field', 'OBJECTID': 'hidden field', 'KOD_UEV': 'hidden field', 'NAZOV_UEV': 'hidden field', 'POSOBNOST': 'hidden field', 'na_vode': 'hidden field', 'EFP': 'header label - visible with data', 'Manazment': 'hidden field', 'Biotop_spo': 'hidden field', });
-lyr_JPRLCHKOKysuce_4.set('fieldLabels', {'fid': 'hidden field', 'Plocha': 'header label - visible with data', 'idjprl': 'header label - visible with data', 'CLHC': 'header label - visible with data', 'LHC': 'header label - visible with data', 'datum': 'hidden field', 'VEK': 'header label - visible with data', 'Druh_pozem': 'header label - visible with data', 'COLP': 'header label - visible with data', 'Výmera_JPR': 'header label - visible with data', 'OBH': 'header label - visible with data', 'KodOBH': 'header label - visible with data', 'Adresa_OBH': 'header label - visible with data', 'OLH': 'header label - visible with data', 'KodOLH': 'header label - visible with data', 'Shape_Leng': 'hidden field', 'Shape_Area': 'hidden field', });
+lyr_Ekologickofunknpriestory_4.set('fieldLabels', {'fid': 'hidden field', 'OBJECTID': 'hidden field', 'KOD_UEV': 'hidden field', 'NAZOV_UEV': 'hidden field', 'POSOBNOST': 'hidden field', 'na_vode': 'hidden field', 'EFP': 'header label - visible with data', 'Manazment': 'hidden field', 'Biotop_spo': 'hidden field', });
 lyr_HraniceCHKOKysuce_5.set('fieldLabels', {'HECTARES': 'no label', 'NAZOV': 'no label', 'VYM_CHKO': 'no label', 'VYM_HA': 'no label', 'AREA': 'no label', 'PERIMETER': 'no label', });
 lyr_zemiaeurpskehovznamu_6.set('fieldLabels', {'fid': 'hidden field', 'OBJECTID': 'hidden field', 'KOD_UEV': 'hidden field', 'NAZOV_UEV': 'header label - visible with data', 'POSOBNOST': 'hidden field', 'VYMERA_HA': 'hidden field', 'Shape_Leng': 'hidden field', 'Shape_Area': 'hidden field', 'na_vode': 'hidden field', });
 lyr_zemiaeurpskehovznamu_6.on('precompose', function(evt) {
